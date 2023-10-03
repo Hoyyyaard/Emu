95330cb0d148f1b90617df2b2d9de534bd2db8e3
hongyan zhi <zhihongyan@gpu002.sxaiit.xyz>
2023-10-03 17:00:58 +0800

    res loss /= len

--- finetune.sh
+++ finetune.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=5,6,7
-python inference.py --lora_finetune --train --mlt_emu --data_path /root/Projects/emu_data_subtask/  --val_data_path /root/Projects/emu_data_subtask_val/ 
+# export CUDA_VISIBLE_DEVICES=5,6,7
+python inference.py --lora_finetune --train --gckpt --clip_norm --batch_size 4 --lr_base 0.005 --epoch 100 --log_dir lrb{0.005}-epo{100}-bs{4}-norm{w}-ckpt{w} --batch_size 4
--- inference.py
+++ inference.py
@@ -323,7 +323,7 @@ def finetune_example(emu_model, args):
                 writer.add_scalar("train_loss_reg_rank0", loss_reg, global_step=global_step)
                 writer.add_scalar("train_loss_cls_rank0", loss_cls, global_step=global_step)
             # REG的loss会比较小
-            loss = loss_cls + loss_reg 
+            loss = ( loss_cls + loss_reg / loss_reg_len) * args.batch_size 
             # if torch.cuda.current_device() == 0:
             #     print(f'Batch 1 infernece takes torch.cuda.memory_reserved : {torch.cuda.memory_reserved(torch.cuda.current_device())/1024**3.:3f} GB')
             
